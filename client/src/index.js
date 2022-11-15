0f588ccb4eff39d7dbd1ce65a528d48236e88792
Kally Cao <kallycao98@gmail.com>
2022-11-14 23:03:21 -0800

    Set up initial file structure, installed dependencies, and configured webpack

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+root.render(<App />);
---
+++ server/server.js
@@ -0,0 +1,1 @@
+//set app to listen to localhost port
